07cccea865a44f4cbf36a9e201485284181fe62f
Lucas Neves <lucas.snowsg@gmail.com>
2023-07-17 10:45:22 -0300

    adding city input

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from selenium.webdriver.common.by import By
 from time import sleep
 import re
 
+cidade = input('Digite a cidade que voce deseja pesquisar: ')
+
 options = Options()
 
 options.add_argument('window-size=900,800')
@@ -16,23 +18,21 @@ navegador.get('https://www.airbnb.com.br/')
 
 sleep(3)
 
-input_place2 = navegador.find_element(By.CLASS_NAME, "_160gnkxa")
+input_place2 = navegador.find_element(By.CLASS_NAME, "_160gnkxa")  #Aceita o cookie
 input_place2.click()
 
 sleep(3)
 
-input_place3 = navegador.find_element(By.CLASS_NAME, "f19g2zq0")
+input_place3 = navegador.find_element(By.CLASS_NAME, "f19g2zq0") #Clica em 'Qualquer lugar'
 input_place3.click()
 
 sleep(3)
 
 input_place = navegador.find_element(By.CLASS_NAME, "iluujbk")
-input_place.send_keys('Luminárias')
+input_place.send_keys(cidade)   #Insere a cidade e pesquisa
 input_place.submit()
 
 sleep(3)
-
-
 
 page_content = navegador.page_source
 
@@ -40,16 +40,14 @@ site = BeautifulSoup(page_content, 'html.parser')
 
 hospedagem = site.find('div', attrs={'itemprop':'itemListElement'})
 
-print(hospedagem.prettify())
-
 hospedagem_nome = hospedagem.find('meta', attrs={'itemprop': 'name'})
+#Adicionar descricao
 hospedagem_url = hospedagem.find('meta', attrs={'itemprop': 'url'})
-print('Nome:', hospedagem_nome['content'])
-print('URL:', hospedagem_url['content'])
-
-hospedagem_detalhes = hospedagem.find('span', attrs={'class':'a8jt5op'})
-valor_por_noite = hospedagem_detalhes.text
+hospedagem_preco = hospedagem.find('span', attrs={'class':'a8jt5op'})
+valor_por_noite = hospedagem_preco.text
 
 valor_numérico = re.search(r'\d+', valor_por_noite).group()
 
+print('Nome:', hospedagem_nome['content'])
+print('URL:', hospedagem_url['content'])
 print('Valor por noite: R$', valor_numérico)
